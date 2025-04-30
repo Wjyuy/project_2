adef703c6884614f3e621e523db59088f6b7fe0c
Chaeyoon-k <codbs9265@gmail.com>
2025-04-30 16:47:24 +0900

    java/xml/js/jsp

---
+++ DefectListDAO.java
@@ -0,0 +1,32 @@
+package com.boot.dao;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.boot.dto.*;
+
+public interface DefectListDAO {
+	public ArrayList<DefectListDTO> defectList();
+//	public void write(HashMap<String, String> param);
+//	public void write(DefectListDTO defectListDTO);
+//	public BoardDTO contentView(HashMap<String, String> param);
+//	public void modify(HashMap<String, String> param);
+//	public void delete(HashMap<String, String> param);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
